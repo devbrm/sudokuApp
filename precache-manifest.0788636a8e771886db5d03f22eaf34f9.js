47ecc96edf484b4cd5f4c2d7a8a9164b0add95ce
Braham Prakash <developer.brm@gmail.com>
2020-09-13 22:01:07 +0530

    Updates

---
+++ precache-manifest.0788636a8e771886db5d03f22eaf34f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd47b98890566383d35ae764b3bc709",
+    "url": "/sudokuApp/index.html"
+  },
+  {
+    "revision": "0fed4edd5ab1310ea12c",
+    "url": "/sudokuApp/static/css/main.4704c35f.chunk.css"
+  },
+  {
+    "revision": "9cb2d4dcdc012ffd88e8",
+    "url": "/sudokuApp/static/js/2.dc0634fb.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/sudokuApp/static/js/2.dc0634fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fed4edd5ab1310ea12c",
+    "url": "/sudokuApp/static/js/main.f3613f39.chunk.js"
+  },
+  {
+    "revision": "e13c3353797c02ae54ea",
+    "url": "/sudokuApp/static/js/runtime-main.e05f1b81.js"
+  }
+]);
